2bf7314e991b40b070d3a70357a37114c74a671e
Alvaro Canepa <bfpdevel@gmail.com>
2024-10-26 09:13:19 -0300

    - Update types on core package

--- packages/core/src/index.d.ts
+++ packages/core/src/index.d.ts
@@ -1,30 +1,3 @@
-import type { Element, ModelOptions } from "pinia-orm";
-import { Model as BaseModel } from "pinia-orm";
-
-interface Model<T extends Element = Element> extends BaseModel {
-  $getOriginal: () => T;
-  $getAttributes: () => T;
-  $toJson: (model?: BaseModel, options?: ModelOptions) => T;
-
-  id: string | number;
-  created_at?: string;
-  updated_at?: string;
-
-  uuid: boolean;
-  baseUrl: string;
-}
-
-declare class Model extends BaseModel {
-  static baseUrl: string;
-  static uuid: boolean;
-
-  static creating<M extends Model = Model>(model: M): void;
-
-  $isNew(): boolean;
-
-  $baseUrl(): string;
-}
-
 export type {
   ApiLinksResponse,
   ApiMetaResponse,
@@ -38,4 +11,3 @@ export type {
   fnUseOrmModel,
   useOrmModel,
 } from "./types";
-export { Model };
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,37 +1,66 @@
-// import { cp } from "node:fs";
+import { writeFileSync } from "node:fs";
 import { defineConfig } from "tsup";
+import npmDts from "npm-dts";
+import { generateDeclaration } from "dets";
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  clean: true,
-  dts: true,
-  experimentalDts: false,
-  format: ["esm", "cjs"],
-  keepNames: true,
-  metafile: false,
-  minify: true,
-  sourcemap: false,
-  splitting: false,
-  outExtension({ format }) {
-    switch (format) {
-      case "cjs": {
-        return { js: ".cjs", dts: ".d.cts" };
-      }
-      case "esm": {
-        return { js: ".mjs", dts: ".d.mts" };
-      }
-      default: {
-        return { js: ".js", dts: ".d.ts" };
-      }
-    }
-  },
-  /* onSuccess(): Promise<void> {
-    cp("./src/index.d.ts", "./dist/index.d.ts", (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
+const { Generator } = npmDts;
 
-    return Promise.resolve();
-  }, */
-});
+const configTsup = (sPackage: string, arImports: string[] = []) => {
+  return defineConfig({
+    entry: ["src/index.ts"],
+    clean: true,
+    dts: false,
+    experimentalDts: false,
+    format: ["esm", "cjs"],
+    keepNames: true,
+    metafile: false,
+    minify: true,
+    sourcemap: false,
+    splitting: false,
+    outExtension({ format }) {
+      switch (format) {
+        case "cjs": {
+          return { js: ".cjs", dts: ".d.cts" };
+        }
+        case "esm": {
+          return { js: ".mjs", dts: ".d.mts" };
+        }
+        default: {
+          return { js: ".js", dts: ".d.ts" };
+        }
+      }
+    },
+    // eslint-disable-next-line require-await
+    async onSuccess() {
+      new Generator({
+        output: "./dist/index.d.ts",
+        force: true,
+      })
+        .generate()
+        .then(async (r: any) => {
+          console.log("Generate types");
+
+          const arImportsList = [
+            "@pinia-orm/axios",
+            "axios",
+            "lodash",
+            "pinia",
+            "pinia-orm",
+          ];
+          const arImportLibs = [...arImportsList, ...arImports];
+          const content = await generateDeclaration({
+            name: `@planetadeleste/${sPackage}/types`,
+            root: process.cwd(),
+            files: ["src/index.d.ts"],
+            imports: arImportLibs,
+          });
+
+          writeFileSync("dist/types.d.ts", content, "utf8");
+          const cmd = `dets src/index.d.ts --name "@planetadeleste/${sPackage}/types" --out dist/types.d.ts --imports ${arImportLibs.map((sItem) => `"${sItem}"`).join(" ")}`;
+          console.log("Run command: " + cmd);
+        });
+    },
+  });
+};
+
+export default configTsup;
